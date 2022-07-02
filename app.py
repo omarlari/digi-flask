7ec018bcabb6c592af0fd5706eb50f7a38f524de
omarlari <omarlari@gmail.com>
2022-07-02 15:43:25 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,49 @@
+import os
+import psycopg2
+from flask import Flask, render_template, request, url_for, redirect, jsonify
+
+# ...
+
+app = Flask(__name__)
+
+def get_db_connection():
+    conn = psycopg2.connect(host='localhost',
+                            database='flask_db',
+                            user=os.environ['DB_USERNAME'],
+                            password=os.environ['DB_PASSWORD'])
+    return conn
+
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM books;')
+    books = cur.fetchall()
+    cur.close()
+    conn.close()
+    return jsonify(books)
+
+@app.route('/init/')
+def init():
+    person = {'name': 'Alice', 'birth-year': 1978}
+    return jsonify(person)
+
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        pages_num = int(request.form['pages_num'])
+        review = request.form['review']
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('INSERT INTO books (title, author, pages_num, review)'
+                    'VALUES (%s, %s, %s, %s)',
+                    (title, author, pages_num, review))
+        conn.commit()
+        cur.close()
+        conn.close()
+        return redirect(url_for('index'))
+
+    return render_template('create.html')
