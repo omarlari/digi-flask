97f9b4a53a65b67932ffc5466bd59b970e6eb4c4
omarlari <omarlari@gmail.com>
2022-07-27 15:29:51 -0700

    fix connection string

--- app.py
+++ app.py
@@ -46,18 +46,14 @@ def index():
 
 @app.route('/seed')
 def seed():
-    conn = psycopg2.connect(
-        host=os.environ['PGHOST'],
-        database=os.environ['PGDATABASE'],
-        user=os.environ['PGUSER'],
-        password=os.environ['PGPASSWORD'])
-        cur = con.cursor()
+    conn = get_db_connection()
+    cur = conn.cursor()
 # Open a cursor to perform database operations
-        cur = conn.cursor()
+        
 
 # Execute a command: this creates a new table
-        cur.execute('DROP TABLE IF EXISTS books;')
-        cur.execute('CREATE TABLE books (id serial PRIMARY KEY,'
+    cur.execute('DROP TABLE IF EXISTS books;')
+    cur.execute('CREATE TABLE books (id serial PRIMARY KEY,'
                                  'title varchar (150) NOT NULL,'
                                  'author varchar (50) NOT NULL,'
                                  'pages_num integer NOT NULL,'
@@ -67,7 +63,7 @@ def seed():
 
 # Insert data into the table
 
-        cur.execute('INSERT INTO books (title, author, pages_num, review)'
+    cur.execute('INSERT INTO books (title, author, pages_num, review)'
             'VALUES (%s, %s, %s, %s)',
             ('A Tale of Two Cities',
              'Charles Dickens',
@@ -76,7 +72,7 @@ def seed():
             )
 
 
-        cur.execute('INSERT INTO books (title, author, pages_num, review)'
+    cur.execute('INSERT INTO books (title, author, pages_num, review)'
             'VALUES (%s, %s, %s, %s)',
             ('Anna Karenina',
              'Leo Tolstoy',
@@ -84,11 +80,11 @@ def seed():
              'Another great classic!')
             )
 
-        conn.commit()
+    conn.commit()
 
-        cur.close()
-        conn.close()
-        return('success!')
+    cur.close()
+    conn.close()
+    return('success!')
 
 @app.route('/create/', methods=('GET', 'POST'))
 def create():
